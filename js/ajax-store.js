45d93ad2bf914385512a6662076dace6921b70ae
Azubuike Motanya <acmotanya@gmail.com>
2017-06-02 10:25:26 -0400

    adding all the crazy changes..

--- js/ajax-store.js
+++ js/ajax-store.js
@@ -470,12 +470,10 @@ function cartHelper()
 }
 
 
-
 //////////////////////////////
 // Get Detail View for Item //
 //////////////////////////////
-function detailView(callback, callback2)
-{
+function detailView(callback, callback2) {
   jQuery("#images, #secondColumn, #addInfo, #thirdColumn").empty();
 
   var dets;
@@ -489,101 +487,109 @@ function detailView(callback, callback2)
   var stock_no = hash[1];
   var productRating = [];
 
-  if (hash.length === 5 ) {
+  if (hash.length === 5) {
     detailString = window.location.hash;
     color = hash[2];
-    type  = hash[3];
+    type = hash[3];
     metal = hash[4];
     sessionStorage.setItem(stock_no, detailString);
-  } else if ( sessionStorage.getItem(stock_no) !== null  && sessionStorage.getItem(stock_no) != "undefined" && sessionStorage.getItem(stock_no).length >= 15 ) {      //  add back if undefined ever comes up again
+  } else if (sessionStorage.getItem(stock_no) !== null && sessionStorage.getItem(stock_no) != "undefined" && sessionStorage.getItem(stock_no).length >= 15) { //  add back if undefined ever comes up again
     dets = sessionStorage.getItem(stock_no).split("+");
     color = dets[2];
-    type  = dets[3];
+    type = dets[3];
     metal = dets[4];
   }
-  
+
   url = "../ljjpgimages-2/" + stock_no + "-2-sm.jpg";
   $.get(url)
-    .done(function() { 
-        secondImage = '<div class="slide" data-thumb="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-sm.jpg"><a href="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-lg.jpg" data-lightbox="gallery-item"><span class="zoom ex1"><img src="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-md.jpg"></span></a></div>';
-    }).fail(function() { 
-        // not exists code
-        console.log("hey guys, there isn't a second image.");
+    .done(function () {
+      secondImage = '<div class="slide" data-thumb="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-sm.jpg"><a href="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-lg.jpg" data-lightbox="gallery-item"><span class="zoom ex1"><img src="https://www.laurajanelle.com/ljjpgimages-2/' + stock_no + '-2-md.jpg"></span></a></div>';
+      populateDetailView(secondImage, color, type, metal, callback, callback2, stock_no);
+    }).fail(function () {
+      // not exists code
+      console.log("hey guys, there isn't a second image.");
+      noSecondImage = '';
+      populateDetailView(noSecondImage, color, type, metal, callback, callback2, stock_no);
     });
+}
 
+/////////////////////////////////////////////////
+// Subroutine to Populate Detail-View Page //
+/////////////////////////////////////////////////
+function populateDetailView(secondImage, color, type, metal, callback, callback2, stock_no) {
   $.ajax({
     type: "GET",
     url: "https://netlink.laurajanelle.com:444/nlhtml/custom/netlink.php?",
-    data: {request_id: "APISTKDTL", stock_no: stock_no, session_no: session_no},
-    success: function(response) {
-
-
-      lines = response.split("\n");
+    data: {
+      request_id: "APISTKDTL",
+      stock_no: stock_no,
+      session_no: session_no
+    },
+    success: function (response) {
       // lines[0] is header row
       // lines[1]+ are data lines
+      lines = response.split("\n");
       fields = lines[1].split("|");
 
-       /* Fill in the pictures for the product */
+      secondColumn = '<div class="row"><div class="col-sm-6 nobottommargin"><span itemprop="productID" class="sku_wrapper" style="font-size: 24px; font-weight: 600;">ITEM # <span class="sku">' + fields[0].replace(/\s+/g, '') + '</span></span></div><div id="mainRatingDiv" class="col-sm-6 nobottommargin"></div></div><div class="line"></div>';
+      secondColumn += '<div class="row"><div class="product-price col-sm-4" style="font-size: 16px; font-weight: 400;"> <ins>COST:&nbsp;' + fields[4] + '</ins></div><div class="col-sm-4 hidden-xs" style="top: 0px; margin: 0px;">MIN: 1</div>';
+      if (fields[3] != ".00") {
+        secondColumn += '<div class="product-rating col-sm-4" style="top: 0px; margin: 0px;">MSRP:&nbsp;' + fields[3] + '</div>';
+      }
+      secondColumn += '</div><div class="clear"></div><div class="line"></div><form class="cart nobottommargin clearfix" method="post" enctype="multipart/form-data"><div class="quantity clearfix">';
+      secondColumn += '<input type="button" value="-" class="minus btn-number" data-type="minus" data-field="quant[1]" onclick="changeQuantity(this)">';
+      secondColumn += '<input type="text" name="quant[1]" step="1" min="1" name="quantity" value="1" title="Qty" size="4" class="qty form-control input-number" id="' + fields[0].replace(/\s+/g, '') + '" />';
+      secondColumn += '<input type="button" value="+" class="plus btn-number" data-type="plus" data-field="quant[1]" onclick="changeQuantity(this)"></div>';
+      secondColumn += '<button type="button" id="add-item" class="add-to-cart button-3d button button-small" onclick="stock_no=\'' + fields[0].trim() + '\'; addItemDetailView();">Add to cart</button><a id="addReviewButton" href="#" data-toggle="modal" data-target="#reviewFormModal" class="add-to-cart button button-3d button-mini hidden-xs" onclick="populateReviewModal(); return false;">Add Review</a></form><div class="clear"></div><div class="line"></div>';
 
-       var pics =  '<div class="fslider" data-pagi="false" data-arrows="false" data-thumbs="true"><div class="flexslider"><div class="slider-wrap" data-lightbox="gallery">';
-           pics += '<div class="slide" data-thumb="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-sm.jpg"><a href="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-lg.jpg" title="' + fields[1] + '" data-lightbox="gallery-item"><span class="zoom ex1"><img src="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-md.jpg" alt="' + fields[1] + '"></span></a></div>';      
-           pics += secondImage;
-           pics += '</div></div></div>';
-        if (fields[7]) {
-          if (fields[7].trim().length === 3) {
-            pics += '<div class="sale-flash">NEW!</div>';
-          }
+      if (fields[8]) {
+        if (fields[8].length !== 0) {
+          secondColumn += '<p>' + fields[8] + '</p>';
+        } else {
+          secondColumn += '<p>' + fields[1] + '</p>';
         }
-   //    secondColumn  = '<div><a href="'+ detailString +'" title="Brand Logo" class="hidden-xs"><img class="image_fade" src="../img/logos/'+ fields[2] +'-logo.png" alt="Brand Logo"></a></div>';
-       secondColumn  = '<div class="col_half nobottommargin"><span itemprop="productID" class="sku_wrapper" style="font-size: 24px; font-weight: 600;">ITEM # <span class="sku">' + fields[0].replace(/\s+/g,'') + '</span></span></div><div id="mainRatingDiv" class="col_half col_last nobottommargin"></div><div class="line"></div>';
-       secondColumn += '<div class="product-price col_one_third" style="font-size: 16px; font-weight: 400;"> <ins>COST:&nbsp;' + fields[4] + '</ins></div><div class="col_one_third hidden-xs" style="top: 0px; margin: 0px;">MIN: 1</div>';
-       if ( fields[3] != ".00" )  {
-         secondColumn += '<div class="product-rating col_one_third col_last" style="top: 0px; margin: 0px;">MSRP:&nbsp;' + fields[3] + '</div>';
-       }
-       secondColumn += '<div class="clear"></div><div class="line"></div><form class="cart nobottommargin clearfix" method="post" enctype="multipart/form-data"><div class="quantity clearfix">';
-       secondColumn += '<input type="button" value="-" class="minus btn-number" data-type="minus" data-field="quant[1]" onclick="changeQuantity(this)">';
-       secondColumn += '<input type="text" name="quant[1]" step="1" min="1" name="quantity" value="1" title="Qty" size="4" class="qty form-control input-number" id="' + fields[0].replace(/\s+/g,'') + '" />';
-       secondColumn += '<input type="button" value="+" class="plus btn-number" data-type="plus" data-field="quant[1]" onclick="changeQuantity(this)"></div>';
-       secondColumn += '<button type="button" id="add-item" class="add-to-cart button-3d button button-small" onclick="stock_no=\'' + fields[0].trim() + '\'; addItemDetailView();">Add to cart</button><a id="addReviewButton" href="#" data-toggle="modal" data-target="#reviewFormModal" class="add-to-cart button button-3d button-mini nomargin hidden-xs" onclick="populateReviewModal(); return false;">Add Review</a></form><div class="clear"></div><div class="line"></div>';
+      }
 
-       if (fields[8]) {
-          if (fields[8].length !== 0) {
-            secondColumn += '<p>' + fields[8] + '</p>';
-          } else {
-            secondColumn += '<p>' + fields[1] + '</p>';
-          }
-       }
+      thirdColumn = '<a title="Brand Logo" class="hidden-xs"><img class="image_fade" src="../img/logos/' + fields[2] + '-logo.png" alt="Brand Logo"></a><div class="divider divider-center"><i class="icon-circle-blank"></i></div>';
 
-       thirdColumn  = '<a href="'+ detailString +'" title="Brand Logo" class="hidden-xs"><img class="image_fade" src="../img/logos/'+ fields[2] +'-logo.png" alt="Brand Logo"></a>';
-       thirdColumn += '<div class="divider divider-center"><i class="icon-circle-blank"></i></div>';
+      info = '<tr><td>Description</td><td>' + fields[1] + '</td></tr>';
+      info += '<tr><td>Dimensions</td><td>' + fields[6] + '</td></tr>';
+      info += '<tr><td>Color</td><td>' + whatColor(color) + '</td></tr>';
+      info += '<tr><td>Type</td><td>' + whatType(type) + '</td></tr>';
+      info += '<tr><td>Look</td><td>' + whatLook(fields[2]) + '</td></tr>';
+      info += '<tr><td>Metal Color</td><td>' + whatMetal(metal) + '</td></tr>';
 
-
-       info =  '<tr><td>Description</td><td>' + fields[1] + '</td></tr>';
-       info += '<tr><td>Dimensions</td><td>' + fields[6] + '</td></tr>';
-       info += '<tr><td>Color</td><td>' + whatColor(color) +'</td></tr>';
-       info += '<tr><td>Type</td><td>' + whatType(type) + '</td></tr>';
-       info += '<tr><td>Look</td><td>' + whatLook(fields[2]) + '</td></tr>';
-       info += '<tr><td>Metal Color</td><td>' + whatMetal(metal) + '</td></tr>';
-       $("#images").html(pics);
-       $("#secondColumn").html(secondColumn);
-       $("#thirdColumn").prepend(thirdColumn);
-       $("#addInfo").html(info);
-     },
-     complete: function () {
-       $('.ex1 img').wrap('<span style="display:inline-block"></span>').css('display', 'block').parent().zoom();
-       setTimeout(function(){
-         SEMICOLON.widget.loadFlexSlider();
-        
-       },500);
-
-        if (callback && typeof(callback) === "function") {
-          callback(stock_no);
+      /* Fill in the pictures for the product */
+      var pics = '<div class="fslider" data-pagi="false" data-arrows="false" data-thumbs="true"><div class="flexslider"><div class="slider-wrap" data-lightbox="gallery">';
+      pics += '<div class="slide" data-thumb="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-sm.jpg"><a href="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-lg.jpg" title="' + fields[1] + '" data-lightbox="gallery-item"><span class="zoom ex1"><img src="https://www.laurajanelle.com/ljjpgimages/' + fields[0] + '-md.jpg" alt="' + fields[1] + '"></span></a></div>';
+      pics += secondImage;
+      pics += '</div></div></div>';
+      if (fields[7]) {
+        if (fields[7].trim().length === 3) {
+          pics += '<div class="sale-flash">NEW!</div>';
         }
-        if (callback2 && typeof(callback2) === "function") {
-          callback2(stock_no);
-        }
-     }
-  });  
+      }
+
+      $("#images").html(pics);
+      $("#secondColumn").html(secondColumn);
+      $("#thirdColumn").prepend(thirdColumn);
+      $("#addInfo").html(info);
+    },
+    complete: function () {
+      $('.ex1 img').wrap('<span style="display:inline-block"></span>').css('display', 'block').parent().zoom();
+      setTimeout(function () {
+        SEMICOLON.widget.loadFlexSlider();
+
+      }, 500);
+
+      if (callback && typeof (callback) === "function") {
+        callback(stock_no);
+      }
+      if (callback2 && typeof (callback2) === "function") {
+        callback2(stock_no);
+      }
+    }
+  });
 }
 
 
@@ -732,8 +738,7 @@ function getReviews(stock_no)
   });
 }
 
-
-  
+ 
 function getAvg(elmt)
 {
   var sum = 0;
@@ -1676,6 +1681,8 @@ function whatMetal(metalCode)
 /////////////////////////
 function search()
 {
+  var oldhash;
+  
   if(event.keyCode == 13) {
     event.preventDefault();
     if ( window.location.hash != "#search" ) {
@@ -1723,7 +1730,6 @@ function filterFunction2(a,b,c,d,e,f,g,h)
       loc_no: h
     },
     success: function(response) {
-      console.log(response);
       $('#shopItems').empty();
       itemRender("shopItems", response);
     },
@@ -1889,7 +1895,7 @@ function getComboFilter() {
     combo = nextCombo;
   }
   var comboFilter = combo.join(', ');
-  console.log(comboFilter);
+
   return comboFilter;
 }
 
